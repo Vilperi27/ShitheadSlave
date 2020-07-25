66db76d83998df3a9a366f99842aebec1ac4d68b
Vilhelm <leppala.vilhelm@gmail.com>
2020-07-25 20:23:50 +0300

    add solo play

--- TeamRandomizer.py
+++ TeamRandomizer.py
@@ -27,7 +27,7 @@ async def shhelp(ctx):
     embed.add_field(name='!team', value='Divide people in two teams (Example. !team player1 player2..)\n--f parameter overrides fair distribution', inline=False)
     embed.add_field(name='!csgo', value='Randomize gun for a round with the money you have (Example. !csgo 3600)', inline=False)
     embed.add_field(name='!pubg', value='Generates a random landing spot for PUBG (Example. !pubg Erangel)\n(Erangel, Miramar, Vikendi, Sanhok, Karakin)', inline=False)
-    embed.add_field(name='!ub', value='Generate random build for user(s), Ultimate Bravery (Example. !ub)', inline=False)
+    embed.add_field(name='!ub', value='Generate random build for user(s), Ultimate Bravery\n(Example. !ub sr player1 player2..)\nMap parameters:\nAram: a\nSummoners rift: sr', inline=False)
 
     await ctx.send(embed=embed)
 
@@ -52,6 +52,17 @@ async def team(ctx, *args):
         await ctx.send(f'Not enough people to divide in two teams.')
         return
 
+
+    t1, t2 = randomize_team(people)
+
+    await ctx.send(f'Team 1: {t1}\nTeam 2: {t2}')
+
+
+def randomize_team(people):
+
+    team_one = []
+    team_two = []
+
     for n in range(20):
         random.shuffle(people)
     
@@ -64,7 +75,7 @@ async def team(ctx, *args):
     t1 = str(team_one).strip("[]").replace("'", "")
     t2 = str(team_two).strip("[]").replace("'", "")
 
-    await ctx.send(f'Team 1: {t1}\nTeam 2: {t2}')
+    return t1, t2
 
 
 @client.command()
@@ -93,23 +104,37 @@ def get_picture(json_partition, prefix):
         var.save('Graphics/' + imgholderstr)
         return var
 
+def is_empty(any_structure):
+    if any_structure:
+        return False
+    else:
+        return True
+
 
 @client.command()
 async def ub(ctx, map, *args):
 
     
-    users = args
+    if(is_empty(args)):
+        user_amount = 1
+        author = str(ctx.message.author).split('#',1)[0]
+        args = args + (author,)
+
     user_amount = len(args)
+    users = args
 
     image_build = Image.new('RGB', ((450, 200*(user_amount))))
     
     mapid = 11
 
-    if(map=='-sr'):
-        mapid = 11
-    elif(map == '-a'):
-        mapid = 12
+    if(user_amount % 2 == 0):
+        t1, t2 = randomize_team(list(users))
 
+    if(map=='sr'):
+        mapid = 11
+    elif(map == 'a'):
+        mapid = 12
+    
     for x in range(user_amount):
         r = requests.post('https://api.ultimate-bravery.net/bo/api/ultimate-bravery/v1/dataset', json = {"map":mapid,"level":30,"language":"en","roles":[0,1,2,3,4],"champions":[266,103,84,12,32,34,1,523,22,136,268,432,53,63,201,51,164,69,31,42,122,131,119,36,245,60,28,81,9,114,105,3,41,86,150,79,104,120,74,420,39,427,40,59,24,126,202,222,145,429,43,30,38,55,10,141,85,121,203,240,96,7,64,89,127,236,117,99,54,90,57,11,21,62,82,25,267,75,111,518,76,56,20,2,61,516,80,78,555,246,133,497,33,421,58,107,92,68,13,113,235,875,35,98,102,27,14,15,72,37,16,50,517,134,223,163,91,44,17,412,18,48,23,4,29,77,6,110,67,45,161,254,112,8,106,19,498,101,5,157,83,350,154,238,115,26,142,143]}, headers = {'Content-type':'application/json'})
         ubdata = r.json()
@@ -208,8 +233,13 @@ async def ub(ctx, map, *args):
 
         image_build.save('datasetbuild.png')
 
-    image_build.close()
     await ctx.send(file=discord.File('datasetbuild.png'))
+    
+    image_build.close()
+
+    print(user_amount)
+    if(user_amount % 2 == 0):
+        await ctx.send(f'Team 1: {t1}\nTeam 2: {t2}')
 
 
 
